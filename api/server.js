721d0573187ce889bcdb6fe9de8443b9f9017979
Syed Masood <84665073+syedsm@users.noreply.github.com>
2024-06-02 10:53:35 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
 
-
+app.get('/',(req,res)=>{
+  res.send("Running")
+})
 app.use('/api', apirouter)
 app.use(express.static('public'))
 app.listen(process.env.PORT, () => { console.log(`Server is running on ${process.env.PORT} `) })
