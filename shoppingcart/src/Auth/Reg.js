45f6e250960215c1ba0bba175281006b4c065a0a
Syed Masood <masoodsyed123786@gmail.com>
2024-06-03 19:11:00 +0530

    fix issue 4

--- shoppingcart/src/Auth/Reg.js
+++ shoppingcart/src/Auth/Reg.js
@@ -26,7 +26,7 @@ function Reg() {
         }
         
         try {
-            fetch('https://shopping-ecart-backend.onrender.com/api/reg', {
+            fetch('/api/reg', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
