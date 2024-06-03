7ca551acc129a2468c8de7c9790835b3e26c1a85
Syed Masood <masoodsyed123786@gmail.com>
2024-06-03 18:58:11 +0530

    fix 2

--- api/server.js
+++ api/server.js
@@ -6,6 +6,13 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+    origin: 'https://shoppingecart.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }));
+
 // console.log(`Connecting to ${process.env.DB_URL}/${process.env.DB_NAME}`);
 mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
