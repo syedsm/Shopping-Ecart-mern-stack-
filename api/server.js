c4947a2fe0a882c35f16c854cb852a63b3f3e8d6
Syed Masood <masoodsyed123786@gmail.com>
2024-06-03 18:16:56 +0530

    1

--- api/server.js
+++ api/server.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const apirouter = require('./router/api')
-const mongoose = require('mongoose')
-require('dotenv').config()
-mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+const express = require('express');
+const mongoose = require('mongoose');
+const apirouter = require('./router/api');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// console.log(`Connecting to ${process.env.DB_URL}/${process.env.DB_NAME}`);
+mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB Atlas', err);
+});
 
 
-app.use('/api', apirouter)
-app.use(express.static('public'))
-app.listen(process.env.PORT, () => { console.log(`Server is running on ${process.env.PORT} `) })
+app.use('/api', apirouter);
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
