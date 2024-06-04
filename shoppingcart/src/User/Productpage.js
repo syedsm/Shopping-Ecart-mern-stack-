fa32d40eded80f174b5f955bf21d4f1004f4dac2
Syed Masood <masoodsyed123786@gmail.com>
2024-06-04 12:05:12 +0530

    make mobile optimized design

--- shoppingcart/src/User/Productpage.js
+++ shoppingcart/src/User/Productpage.js
@@ -5,15 +5,18 @@ import { Link } from "react-router-dom";
 function Usersproduct() {
     const [products, setproducts] = useState([]);
     const [message, setmessage] = useState('');
-    const { isDarkMode } = useContext(Contextapi)
+    const { isDarkMode } = useContext(Contextapi);
+
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/api/instockproducts`, {}).then((result) => result.json()).then((data) => {
-            if (data.status === 200) {
-                setproducts(data.apiData);
-            } else {
-                setmessage(data.message);
-            }
-        });
+        fetch(`${process.env.REACT_APP_API_URL}/api/instockproducts`)
+            .then((result) => result.json())
+            .then((data) => {
+                if (data.status === 200) {
+                    setproducts(data.apiData);
+                } else {
+                    setmessage(data.message);
+                }
+            });
     }, []);
 
     const { cart, setCart } = useContext(Contextapi);
@@ -40,15 +43,14 @@ function Usersproduct() {
         <>
             <section id="product">
                 {message && <div className="alert alert-danger" role="alert">{message}</div>}
-                <div className="container mt-3 " >
-                    <div className="row justify-content-center " >
+                <div className="container mt-3">
+                    <div className="row justify-content-center">
                         {products.map((item) => (
-                            <div key={item._id} className="col-md-4 mb-4" >
+                            <div key={item._id} className="col-lg-4 col-md-6 col-sm-12 mb-4">
                                 <div className={`card h-100 ${isDarkMode ? 'bg-dark text-white' : 'bg-light text-dark'}`}>
-                                    <img src={`../productimages/${item.img}`} className="card-img-top" alt="..." />
+                                    <img src={`../productimages/${item.img}`} className="card-img-top" alt={item.name} />
                                     <div className="card-body">
                                         <h5 className={`card-title ${isDarkMode ? 'text-white' : 'text-dark'}`}>{item.name}</h5>
-
                                         <p className="card-text">{item.desc}</p>
                                         <p className="card-text">${item.price}</p>
                                         <div className="d-grid gap-2">
