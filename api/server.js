997e5d7db214babf13d65012438846ff83e8a394
Syed Masood <masoodsyed123786@gmail.com>
2024-08-15 11:05:02 +0530

    changes

--- api/server.js
+++ api/server.js
@@ -6,15 +6,15 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-// const cors = require("cors")
+const cors = require("cors")
 
-// app.use(cors({
-//     // origin: 'https://shoppingecart.vercel.app',
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true
-// }));
+app.use(cors({
+    origin: 'https://shoppingecart.vercel.app',
+    // origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 
 // console.log(`Connecting to ${process.env.DB_URL}/${process.env.DB_NAME}`);
