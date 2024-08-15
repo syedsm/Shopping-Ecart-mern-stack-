bee5cdda9b5678a20fbc2b30c2406f18f0140b91
Syed Masood <masoodsyed123786@gmail.com>
2024-08-15 10:40:14 +0530

    add

--- frontend-shoppingcart/vite.config.js
+++ frontend-shoppingcart/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL,
+          target: env.VITE_SERVER_URL,
           changeOrigin: true,
           secure: false,
         },
