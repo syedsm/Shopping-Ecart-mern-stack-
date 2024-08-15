e04d24204925ff42c645e345daf891cea2ef02f3
Sayyed Masud <84665073+syedsm@users.noreply.github.com>
2024-08-15 15:03:06 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,16 +6,16 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-const cors = require("cors")
+// const cors = require("cors")
 
-app.use(cors({
-    // origin: 'https://shoppingecart.vercel.app',
-    origin:"https://shopping-ecart-mern-stack.vercel.app",
-    // origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+// app.use(cors({
+//     // origin: 'https://shoppingecart.vercel.app',
+//     origin:"https://shopping-ecart-mern-stack.vercel.app",
+//     // origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true
+// }));
 
 
 // console.log(`Connecting to ${process.env.DB_URL}/${process.env.DB_NAME}`);
